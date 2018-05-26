0f418b6700d77e08371355b16bdb7ccb2deede0f
skoval-dev <dev.skoval@gmail.com>
2018-05-26 20:10:11 +0300

    Added user route for admin manipulations

--- server/server.js
+++ server/server.js
@@ -17,6 +17,41 @@ const          port  = process.env.PORT;
 
 app.use(body_parser.json());
 
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+    user.save().then(() => {
+        return user.generate_auth_token();
+    }).then((token) => {
+        res.header('x-auth', token).status(200).send(user);
+    }).catch((err) => {
+        res.status(400).send({message: err.message, success: false});
+    })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    User.find_by_credentials(body.email, body.password).then((user) => {
+        user.generate_auth_token().then((token) => {
+            res.header('x-auth', token).status(200).send(user);
+        });
+
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.delete("/users/me/token", authenticate, (req, res) => {
+    req.user.remove_token(req.token).then(() => {
+        res.status(200).send({message: "The token was deleted", success: true})
+    }).catch((err) => {
+        res.status(400).send({message: err.message, success: false});
+    });
+});
 
 
 app.listen(port, () => {
