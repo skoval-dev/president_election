68f94d586fa9e22d6c24f4f6a2198411a4c76d7d
skoval-dev <dev.skoval@gmail.com>
2018-05-26 17:20:17 +0300

    Created schema for candidate

---
+++ server/models/candidate.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const candidateSchema = new Schema({
+    full_name: {
+        type: String,
+        required: true,
+        minlength: [6, 'Text should contains at least 6 characters'],
+        trim: true
+    },
+    birth_day: {
+        type: Date,
+        required: true,
+    },
+    intro: {
+        type: String,
+        required: true,
+    },
+    votes_count: {
+        type: Number,
+        required: true
+    }
+});
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+
+module.exports = {
+    Candidate
+};
