fcba5f3cd635df39ea8750012b2ccc499486bb15
skoval-dev <dev.skoval@gmail.com>
2018-05-27 14:57:56 +0300

    Added new static method for electorate instance

--- server/models/electorate.js
+++ server/models/electorate.js
@@ -22,6 +22,11 @@ electorateSchema.statics.create_electorates = function(count, election_id){
     return electorate.insertMany(arr_electorate);
 };
 
+electorateSchema.statics.made_vote = function(id){
+    const electorate = this;
+    return electorate.findOneAndUpdate({_id: id}, {$set: {"voted": true}}, {new: true});
+};
+
 const Electorate = mongoose.model('Electorate', electorateSchema);
 
 module.exports = {
