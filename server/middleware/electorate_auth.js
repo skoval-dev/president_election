6bee064e71d4baeb831cf6cdfeeb1908dda4fdc6
skoval-dev <dev.skoval@gmail.com>
2018-05-27 14:57:21 +0300

    Create auth middleware for electorates

---
+++ server/middleware/electorate_auth.js
@@ -0,0 +1,21 @@
+const {Electorate} = require('./../models/electorate');
+
+const electorate_auth = (req, res, next) => {
+    const id = req.header('x-elec-id');
+    if(id && !ObjectID.isValid(id)) {
+        return res.status(401).send({message: "You are not authorised!", success: false});
+    }
+    Electorate.findOne({_id: id, voted: false}).then((electorate) => {
+        if(!electorate){
+            return Promise.reject('You can not make vote!');
+        }
+
+        req.electorate = electorate;
+        req.token = id;
+        next();
+    }).catch((err) => {
+        res.status(401).send({message: err, success: false});
+    });
+};
+
+module.exports = {electorate_auth};
