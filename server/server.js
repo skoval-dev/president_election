13736e4ae72b23d3e07f0ee01489520749de23bd
skoval-dev <dev.skoval@gmail.com>
2018-05-27 16:00:54 +0300

    Added routes for candidate instance

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const    {Election}  = require('./models/election');
 const  {Electorate}  = require('./models/electorate');
 const        {User}  = require('./models/user');
 const {authenticate} = require('./middleware/authenticate');
+const {electorate_auth} = require('./middleware/electorate_auth');
 
 const           app  = express();
 const          port  = process.env.PORT;
@@ -19,6 +20,9 @@ app.use(body_parser.json());
 
 app.post('/users', (req, res) => {
     let body = _.pick(req.body, ['email', 'password']);
+    if(_.isEmpty(body)){
+        return res.status(403).send({message: `The fields are not suitable to process`, success: false});
+    }
     let user = new User(body);
     user.save().then(() => {
         return user.generate_auth_token();
@@ -35,6 +39,9 @@ app.get('/users/me', authenticate, (req, res) => {
 
 app.post('/users/login', (req, res) => {
     let body = _.pick(req.body, ['email', 'password']);
+    if(_.isEmpty(body)){
+        return res.status(403).send({message: `The fields are not suitable to process`, success: false});
+    }
     User.find_by_credentials(body.email, body.password).then((user) => {
         user.generate_auth_token().then((token) => {
             res.header('x-auth', token).status(200).send(user);
@@ -55,7 +62,9 @@ app.delete("/users/me/token", authenticate, (req, res) => {
 
 app.post('/election', authenticate, (req, res) => {
     let body = _.pick(req.body, ['name', 'start_date', 'end_date', 'state', 'electorate_count', 'tour']);
-    
+    if(_.isEmpty(body)){
+        return res.status(403).send({message: `The fields are not suitable to process`, success: false});
+    }
     if(body['end_date'] <= body['start_date']){
         res.status(400).send({'message': 'The end date should be more than start date'});
     } else {
@@ -135,7 +144,9 @@ app.patch('/election/:id', authenticate, (req, res) => {
     }
     //Filter props from body based on provided in arr
     let body = _.pick(req.body, ['state', 'name']);
-
+    if(_.isEmpty(body)){
+        return res.status(403).send({message: `The fields are not suitable for updates`, success: false});
+    }
     Election.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).then((election) => {
         if(!election){
             return res.status(404).send({message: `There is not found election by id <${id}>`, success: false});
@@ -146,19 +157,131 @@ app.patch('/election/:id', authenticate, (req, res) => {
     });
 });
 
-app.patch('/electorate/:id', authenticate, (req, res) => {
+app.get('/electorate/login/:id', (req, res) => {
+    let id = req.params && req.params.id;
+    if(id && !ObjectID.isValid(id)) {
+        return res.status(404).send({message: `The id: <${id}> is not valid!`, success: false})
+    }
+    Electorate.findOne({_id: id}).then((electorate) => {
+        if(!electorate){
+           return res.status(401).send({message:"You don't have access to current election"});
+        }
+        res.header('x-elec-id', id).status(200).send(electorate);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.post('/candidate', authenticate, (req, res) => {
+    let body = _.pick(req.body, ['full_name', 'birth_day', 'intro', '_election']);
+    if(_.isEmpty(body)){
+        return res.status(403).send({message: `The fields are not suitable to process`, success: false});
+    }
+    const candidate = new Candidate(body);
+    candidate.save().then((doc) => {
+        res.status(200).send(doc);
+    }).catch((err) => {
+        res.status(400).send(err.message);
+    });
+
+});
+
+app.get('/candidate', (req, res) => {
+	Candidate.find().sort('full_name').select('full_name birth_day intro _election votes_count').then((candidate) => {
+		if(candidate.length === 0){
+			throw new Error('There is not available candidate');
+        }
+        
+		res.status(200).send({candidate, success: true});
+	}).catch((err) => {
+		res.status(400).send({success: false, message: err.message});
+	});
+});
+
+app.get('/candidate/id/:id', (req, res) => {
+    let id = req.params && req.params.id;
+    if(id && !ObjectID.isValid(id)) {
+        return res.status(404).send({message: `The id: <${id}> is not valid!`, success: false})
+    }
+    Candidate.findOne({_id: id}).select('full_name birth_day intro _election').then((candidate) => {
+        if(!candidate){
+            return res.status(404).send({message: `There is not found candidate by id <${id}>`, success: false});
+        }
+       
+		res.status(200).send({candidate, success: true});
+    }).catch((err) => {
+        res.status(400).send({message: err.message, success: false});
+    });
+});
+
+app.get('/candidate/election/:id', (req, res) => {
+    let id = req.params && req.params.id;
+
+    if(id && !ObjectID.isValid(id)) {
+        return res.status(404).send({message: `The id: <${id}> is not valid!`, success: false})
+    }
+    Candidate.find({_election: id}).select('full_name birth_day intro _election').then((candidate) => {
+        if(!candidate){
+            return res.status(404).send({message: `There is not found candidate by election _id <${id}>`, success: false});
+        }
+        res.status(200).send({candidate, success: true});
+    }).catch((err) => {
+        res.status(400).send({message: err.message, success: false});
+    });
+});
+
+app.delete('/candidate/:id', authenticate, (req, res) => {
+    let id = req.params && req.params.id;
+    if(id && !ObjectID.isValid(id)) {
+        return res.status(404).send({message: `The id: <${id}> is not valid!`, success: false})
+    }
+    Candidate.findOneAndRemove({_id: id}).select('full_name birth_day intro _election').then((candidate) => {
+        if(!candidate){
+            return res.status(404).send({message: `There are not found candidate by id <${id}>`, success: false});
+        }
+        res.status(200).send({candidate, success: true});
+    }).catch((err) => {
+        res.status(400).send({message: err.message, success: false});
+    });
+});
+
+app.patch('/candidate/:id', authenticate, (req, res) => {
     let id = req.params && req.params.id;
     if(id && !ObjectID.isValid(id)) {
         return res.status(404).send({message: `The id: <${id}> is not valid!`, success: false})
     }
     //Filter props from body based on provided in arr
-    let body = _.pick(req.body, ['voted']);
-
-    Electorate.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).then((electorate) => {
-        if(!electorate){
-            return res.status(404).send({message: `There is not found electorate by id <${id}>`, success: false});
+    let body = _.pick(req.body, ['intro', 'full_name', 'birth_day']);
+    if(_.isEmpty(body)){
+        return res.status(403).send({message: `The fields are not suitable for updates`, success: false});
+    }
+    Candidate.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).select('full_name birth_day intro _election').then((candidate) => {
+        if(!candidate){
+            return res.status(404).send({message: `There is not found candidate by id <${id}>`, success: false});
         }
-        res.status(200).send({electorate, success: true});
+        res.status(200).send({candidate, success: true});
+    }).catch((err) => {
+        res.status(400).send({message: err.message, success: false});
+    });
+});
+
+app.patch('/candidate/add_vote/:id', electorate_auth, (req, res) => {
+    let id = req.params && req.params.id;
+    if(id && !ObjectID.isValid(id)) {
+        return res.status(404).send({message: `The id: <${id}> is not valid!`, success: false})
+    }
+    //Filter props from body based on provided in arr
+    let body = _.pick(req.body, ['add_vote']);
+    if(!body.add_vote && !body.add_vote === true){
+        return res.status(403).send({message: `The fields are not suitable to operate`, success: false});
+    }
+    Candidate.findOneAndUpdate({_id: id}, {$inc: {votes_count:1}}, {new: true}).select('full_name birth_day intro _election').then((candidate) => {
+        if(!candidate){
+            return res.status(404).send({message: `There is not found candidate by id <${id}>`, success: false});
+        }
+        Electorate.made_vote(req.electorate.token).then((doc) => {
+            res.status(200).send({candidate, success: true});
+        });
     }).catch((err) => {
         res.status(400).send({message: err.message, success: false});
     });
