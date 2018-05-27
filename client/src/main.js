4a3b81e5217603cb3bd4784704f3548d2c6aa9db
skoval-dev <dev.skoval@gmail.com>
2018-05-27 22:20:36 +0300

    Added bootstrap-vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
+import store from './store/store.js'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
