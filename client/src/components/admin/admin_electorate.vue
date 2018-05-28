32209589a903692c04d05d9715f56818a07f177e
skoval-dev <dev.skoval@gmail.com>
2018-05-28 21:24:05 +0300

    Created additional admin components

---
+++ client/src/components/admin/admin_electorate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="electorate">
+    <h1>Test 2!</h1>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'admin-electorate'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- client/src/components/admin/admin_nav.vue
+++ client/src/components/admin/admin_nav.vue
@@ -1,12 +1,20 @@
 <template>
-    <ul>
-        <li> <a href="#"> Test 1 </a> </li>
-        <li> <a href="#"> Test 2 </a> </li>
-        <li> <a href="#"> Test 3 </a> </li>
-    </ul>
+    <b-nav pills>
+        <b-nav-item to="/admin/election" active-class='active'>Election</b-nav-item>
+        <b-nav-item to="/admin/candidate">Candidate</b-nav-item>
+        <b-nav-item to="/admin/electorate">Electorate</b-nav-item>
+        <b-col md="2" offset-md="6"><b-nav-item @click="logout">Logout</b-nav-item></b-col>
+    </b-nav>
 </template>
 <script>
     export default {
-        name: 'admin-nav'
+        name: 'admin-nav',
+        methods: {
+            logout: function () {
+                this.$store.dispatch('AUTH_LOGOUT').then(() => {
+                    this.$router.push({path: '/login'})
+                })
+            }
+        }
     }
 </script>
