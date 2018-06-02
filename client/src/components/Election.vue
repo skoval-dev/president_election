ce6d8478f6b11f6655cf482ebbc3b97a313914d4
skoval-dev <dev.skoval@gmail.com>
2018-06-02 21:49:19 +0300

    Added Election Results Visualization using vue-chart

--- client/src/components/Election.vue
+++ client/src/components/Election.vue
@@ -2,13 +2,19 @@
   <v-app>
     <customer-header></customer-header>
     <v-content>
-      <v-container>
+      <v-container v-if="!election_finished">
         <h1 class="text-xs-center">Welcome to Ukraine President Election</h1>
         <h2 class="text-xs-center mb-3">Your choice is very important for us!</h2>
         <stepper></stepper>
       </v-container>
     </v-content>
-     <customer-footer></customer-footer>
+    <v-container v-if="election_finished">
+      <h1 class="text-xs-center">Ukraine Election 2018 is finished</h1>
+      <h3 class="text-xs-center">The winner of 2018's election is Donald Dak</h3>
+      <h3 class="text-xs-center mb-5">You can see the election results on the chart below: </h3>
+      <results></results>
+    </v-container>
+    <customer-footer></customer-footer>
   </v-app>
 </template>
 
@@ -16,17 +22,21 @@
   import footer from './election/footer';
   import header from './election/header';
   import stepper from './election/stepper';
+  import results from './election/results';
   export default {
     name: 'Election',
     components: {
       'customer-footer': footer,
       'customer-header': header,
-      'stepper': stepper
+      'stepper': stepper,
+      'results': results
     },
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        election_finished: localStorage.getItem("election_finished") == "true",
       }
     }
   }
+
 </script>
---
+++ client/src/components/election/results.vue
@@ -0,0 +1,29 @@
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  mounted () {
+    this.renderChart({
+      labels: ['Donald Dak', 'Mikki Mouse', 'Pistol Pete'],
+      datasets: [
+        {
+            backgroundColor: [
+            '#41B883',
+            '#E46651',
+            '#00D8FF'
+            ],
+          data: [45, 27.5, 27.5]
+        }
+      ]
+    }, {
+      responsive: true, 
+      maintainAspectRatio: false,
+      pieceLabel: {
+        mode: 'percentage',
+        precision: 1
+      }
+    })
+ }
+}
+</script>
