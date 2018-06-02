4fa8646ebe2768fa9faa7c023163fbaee65d08bb
skoval-dev <dev.skoval@gmail.com>
2018-06-02 19:41:16 +0300

    Added few demo candidates

--- client/src/components/admin/admin_candidate.vue
+++ client/src/components/admin/admin_candidate.vue
@@ -139,14 +139,30 @@
     methods: {
       initialize() {
         this.candidates = [{
-          _id: '5b0aaae93930200c68a7cd35',
+          _id: '5b0aa1903bc3662f04cca8c6',
           full_name: 'Donald Dak',
           birth_day: '09-06-1934',
           intro: "Donald Duck is a cartoon character created in 1934 at Walt Disney Productions. Donald is an anthropomorphic white duck with a yellow-orange bill, legs, and feet. He typically wears a sailor shirt and cap with a bow tie. Donald is most famous for his semi-intelligible speech and his mischievous and temperamental personality. Along with his friend Mickey Mouse, Donald is one of the most popular Disney characters and was included in TV Guide's list of the 50 greatest cartoon characters of all time in 2002. He has appeared in more films than any other Disney character,[2] and is the most published comic book character in the world outside of the superhero genre.",
           _election: '5b0aa60f1aac413fb0dc69ed',
-          votes_count: 0
-        }]
-      },
+          votes_count: 2
+        },
+        {
+          _id: '5b0aa6471aac413fb0dc69f3',
+          full_name: 'Mikki Mouse',
+          birth_day: '18-11-1928',
+          intro: "Mickey Mouse is a funny animal cartoon character and the mascot of The Walt Disney Company. He was created by Walt Disney and Ub Iwerks at the Walt Disney Studios in 1928. An anthropomorphic mouse who typically wears red shorts, large yellow shoes, and white gloves, Mickey is one of the world's most recognizable characters.",
+          _election: '5b0aa60f1aac413fb0dc69ed',
+          votes_count: 1
+        },
+        {
+          _id: '5b0aaae93930200c68a7cd35',
+          full_name: 'Pistol Pete',
+          birth_day: '12-02-1925',
+          intro: "Pete (also called Peg-Leg Pete, Pistol Pete and Black Pete, among other names) is an anthropomorphic cartoon character created in 1925 by Walt Disney and Ub Iwerks. He is a character of The Walt Disney Company and often appears as a nemesis and the main antagonist in Mickey Mouse universe stories. He was originally an anthropomorphic bear but with the advent of Mickey Mouse in 1928, he was defined as a cat. Pete is the oldest continuing Disney character, having debuted three years before Mickey Mouse in the cartoon Alice Solves the Puzzle (1925).",
+          _election: '5b0aa60f1aac413fb0dc69ed',
+          votes_count: 1
+        }
+      ]},
 
       editItem(item) {
         this.editedIndex = this.candidates.indexOf(item)
